429c81aeea38cad3f7bc94bd074ee999582df029
Aghason Emmanuel <aghason.emmanuel@gmail.com>
2026-02-22 01:58:05 +0100

    Some changes

--- backend/app/services/api.py
+++ backend/app/services/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import auth, items
+from app.services import auth, items
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
